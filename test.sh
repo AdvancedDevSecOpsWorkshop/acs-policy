af7b5bf22f70bac82a57425388faa4a1fa1b051a
Gerald Nunn <gerald.b.nunn@gmail.com>
2025-01-18 10:20:15 -0800

    Update to use stackrox collection for configuration, mich simpler

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #Script used to test playbook locally
 
-PASSWORD=xxxx
-CENTRAL_ENDPOINT=xxx
+PASSWORD="gtEACeODadniqxdm3rjMwQZ0N"
+CENTRAL_ENDPOINT="central-stackrox.apps.cluster-vs25l.vs25l.sandbox2919.opentlc.com"
+SUBDOMAIN=apps.cluster-vs25l.vs25l.sandbox2919.opentlc.com
 
-ansible-playbook acs.yaml -e username=admin -e password=$PASSWORD -e api_endpoint=$CENTRAL_ENDPOINT
+ansible-playbook acs.yaml -e username=admin -e password=$PASSWORD -e api_endpoint=$CENTRAL_ENDPOINT -e subdomain=$SUBDOMAIN
